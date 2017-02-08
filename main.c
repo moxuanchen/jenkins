0782fd0e40c74eb4f0c1032996b1f99fde48aecf
Yunchao Chen <chenyunchao@xiaoyezi.com>
2017-02-08 18:49:35 +0800

    update main.c
    Signed-off-by: Yunchao Chen <chenyunchao@xiaoyezi.com>

--- main.c
+++ main.c
@@ -5,9 +5,15 @@ void say_hello_to_jenkins(void)
     printf("Hello Jenkins, nice to meet you...\n");
 }
 
+void say_hello_to_jenkins_again(void)
+{
+    printf("Hello Jenkins, glad to see you again...\n");
+}
+
 int main(int argc, char *argv[])
 {
     say_hello_to_jenkins();
+    say_hello_to_jenkins_again();
     return 0;
 }
 
